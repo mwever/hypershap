ef9216586aa758da6c7a657ee5cb6dddeb026009
Maximilian <maximilian.muschalik@gmail.com>
2024-08-21 12:53:03 +0200

    added utils and interaction_quantification.py

---
+++ interaction_quantification.py
@@ -0,0 +1,165 @@
+"""This module contains function to quantify the level of interactions in the games."""
+
+import copy
+from typing import Optional
+
+import numpy as np
+import scipy
+
+import shapiq
+from utils import setup_game
+
+
+def approximate_game_with_interactions(
+    game: shapiq.Game,
+    indices: Optional[list[str]] = None,
+) -> dict[str, dict[int, shapiq.InteractionValues]]:
+    """Approximates the game with different interaction indices and orders.
+
+    Args:
+        game: The game to approximate.
+        indices: The indices to approximate the game with. Default is `None`.
+
+    Returns:
+        A dictionary containing the approximated interaction values with the indices and orders as
+        keys.
+    """
+    if indices is None:
+        indices = ["k-SII", "FSII", "FBII", "STII"]
+    computer = shapiq.ExactComputer(n_players=game.n_players, game_fun=game)
+    approximations = {}
+    for index in indices:
+        approximations[index] = {}
+        for order in range(1, game.n_players + 1):
+            approximations[index][order] = computer(index=index, order=order)
+    return approximations
+
+
+def _convert_game_to_interaction(exact_computer: shapiq.ExactComputer) -> shapiq.InteractionValues:
+    """Converts the exact game values to interaction values.
+
+    Args:
+        exact_computer: The exact computer to convert the game values from.
+
+    Returns:
+        The interaction values.
+    """
+    baseline_value = exact_computer.game_values[exact_computer.coalition_lookup[tuple()]]
+    game_values = shapiq.InteractionValues(
+        values=exact_computer.game_values,
+        index="Moebius",
+        interaction_lookup=exact_computer.coalition_lookup,
+        n_players=exact_computer.n,
+        min_order=0,
+        max_order=exact_computer.n,
+        baseline_value=baseline_value,
+    )
+    return game_values
+
+
+def _get_weight(n: int, coalition_size: int, uniform_weights: bool = False) -> float:
+    """Computes the weight for the given coalition size.
+
+    Args:
+        n: The number of players.
+        coalition_size: The size of the coalition.
+        uniform_weights: Whether to use uniform weights or a Shapley kernel. Default is `False`
+            (Shapley kernel).
+
+    Returns:
+        The weight for the given coalition size.
+    """
+    if uniform_weights:
+        return (1 / 2) ** n
+    if coalition_size == n or coalition_size == 0:
+        sv_weight = 1
+    else:
+        sv_weight = scipy.special.binom(n, coalition_size) * coalition_size * (n - coalition_size)
+        sv_weight = (n - 1) / sv_weight
+    return sv_weight
+
+
+def _get_approximation_weights(n: int, uniform_weights: bool = False) -> np.ndarray:
+    """Computes the weights for the approximations.
+
+    Args:
+        n: The number of players.
+        uniform_weights: Whether to use uniform weights or a Shapley kernel. Default is `False`
+            (Shapley kernel).
+
+    Returns:
+        The weights for the approximations as a numpy array.
+    """
+    approximation_weights = np.zeros(2**n, dtype=float)
+    grand_coalition_set = set(range(n))
+    for coalition_pos, coalition in enumerate(shapiq.powerset(grand_coalition_set)):
+        approximation_weights[coalition_pos] = _get_weight(n, len(coalition), uniform_weights)
+    return approximation_weights
+
+
+def get_approximation_error(
+    approximations: dict[str, dict[int, shapiq.InteractionValues]],
+    game_values: shapiq.InteractionValues,
+    uniform_weights: bool = False,
+) -> dict[str, dict[int, float]]:
+    """Computes the approximation error for the approximated interaction values.
+
+    Args:
+        approximations: The approximated interaction values.
+        game_values: The exact game values.
+        uniform_weights: Whether to use uniform weights or a Shapley kernel. Default is `False`
+            (Shapley kernel).
+
+    Returns:
+        A dictionary containing the approximation errors for the approximated interaction values.
+    """
+    weights = _get_approximation_weights(game_values.n_players, uniform_weights=uniform_weights)
+    game_values_copy = copy.deepcopy(game_values)
+    approximation_errors = {}
+    for index, approximations_order in approximations.items():
+        approximation_errors[index] = {}
+        for order, approximation in approximations_order.items():
+            # get the approximation values
+            approx = copy.deepcopy(approximations[index][order])
+            approx_arr = np.zeros(len(game_values.values))
+            approx_arr[: len(approx.values)] = approx.values  # padded with zeros
+
+            # get the difference between the game and si values
+            game_values_copy.index = approx.index  # workaround to compare
+            difference = approx - game_values_copy  # take the difference between game and si
+
+            # compute the weighted r2 score
+            error = np.sum(weights * difference.values**2)
+            weighted_average = np.sum(weights * approx_arr) / np.sum(weights)
+            total_sum_of_squares = np.sum(weights * (approx_arr - weighted_average) ** 2)
+            r2_score = 1 - error / total_sum_of_squares
+
+            approximation_errors[index][order] = r2_score
+    return approximation_errors
+
+
+def evaluate_game(
+    game: shapiq.Game, indices: Optional[list[str]] = None, uniform_weights: bool = False
+) -> None:
+    """Evaluates the game.
+
+    Args:
+        game: The game to evaluate.
+        indices: The indices to approximate the game with. Default is `None`.
+        uniform_weights: Whether to use uniform weights or a Shapley kernel. Default is `False`
+            (Shapley kernel).
+    """
+    computer = shapiq.ExactComputer(n_players=game.n_players, game_fun=game)
+    game_values = _convert_game_to_interaction(computer)
+    approximations = approximate_game_with_interactions(game, indices=indices)
+    approximation_errors = get_approximation_error(approximations, game_values, uniform_weights)
+    print("R2 Scores per Order:", approximation_errors)
+
+
+if __name__ == "__main__":
+
+    hpo_game, _, _ = setup_game(
+        game_type="local", benchmark_name="rbv2_ranger", normalize_loaded=True
+    )
+
+    evaluate_game(hpo_game, indices=["FSII"], uniform_weights=False)
---
+++ utils.py
@@ -0,0 +1,187 @@
+"""This module contains utility functions for running the experiments."""
+
+import os
+from typing import Optional
+
+from tqdm import tqdm
+from yahpo_gym import benchmark_set
+
+import shapiq
+from hpo_games import (
+    GlobalHyperparameterImportanceGame,
+    LocalHyperparameterImportanceGame,
+    UniversalHyperparameterImportanceGame,
+    UniversalLocalHyperparameterImportanceGame,
+)
+
+GAME_STORAGE_DIR = "game_storage"
+os.makedirs(GAME_STORAGE_DIR, exist_ok=True)
+
+
+__all__ = ["setup_game"]
+
+
+def _find_optimal_configuration(
+    benchmark: benchmark_set.BenchmarkSet,
+    metric: str = "acc",
+    random_state: Optional[int] = 42,
+    n_configs: int = 1000,
+) -> tuple[dict, float]:
+    """Find the optimal configuration for the given benchmark.
+
+    Args:
+        benchmark: The benchmark set.
+        metric: The metric to optimize. Default is `"acc"`.
+        random_state: The random state. Default is `42`.
+        n_configs: The number of configurations to sample. Default is `1000`.
+
+    Returns:
+        A tuple containing the optimal configuration and its value.
+    """
+
+    opt_config, opt_config_value = None, None
+    search_space = benchmark.get_opt_space(drop_fidelity_params=False, seed=random_state)
+    configurations = search_space.sample_configuration(n_configs)
+    for config in configurations:
+        config_dict = config.get_dictionary()
+        config_value = benchmark.objective_function(config_dict)[0][metric]
+        if opt_config_value is None or config_value > opt_config_value:
+            opt_config, opt_config_value = config_dict, config_value
+    return opt_config, opt_config_value
+
+
+def _get_game_name(
+    game_type: str,
+    benchmark: str,
+    **kwargs,
+) -> str:
+    """Get the name of the game.
+
+    Args:
+        game_type: The game to get the name for. Available games are:
+            - universal: The `UniversalHyperparameterImportanceGame`.
+            - global: The `GlobalHyperparameterImportanceGame`.
+            - local: The `LocalHyperparameterImportanceGame`.
+            - universal-local: The `UniversalLocalHyperparameterImportanceGame`.
+        benchmark_name: The name of the benchmark.
+        **kwargs: Additional keyword arguments to be passed to the game name.
+
+    Returns:
+        The name of the game.
+    """
+    game_name = game_type
+    game_name += f"_{benchmark}"
+    for key, value in kwargs.items():
+        game_name += f"_{key}={value}"
+    return game_name
+
+
+def setup_game(
+    game_type: str,
+    benchmark_name: str,
+    metric: str = "acc",
+    instance: Optional[int] = None,
+    random_state: Optional[int] = 42,
+    n_configs: int = 1000,
+    pre_compute: bool = False,
+    verbose: bool = False,
+    normalize_loaded: bool = True,
+) -> tuple[shapiq.Game, str, list[str]]:
+    """Sets up the hyperparameter importance game.
+
+    Available game types are:
+        - universal: The `UniversalHyperparameterImportanceGame`.
+        - global: The `GlobalHyperparameterImportanceGame`.
+        - local: The `LocalHyperparameterImportanceGame`.
+        - universal-local: The `UniversalLocalHyperparameterImportanceGame`.
+
+    Args:
+        game_type: The type of game to set up.
+        benchmark_name: The name of the benchmark.
+        metric: The metric to optimize. Default is `"acc"`.
+        instance: The instance to use. If None, the first instance is used. Default is `None`.
+        random_state: The random state. Default is `42`.
+        n_configs: The number of configurations to sample. Default is `1000`.
+        pre_compute: Whether to pre-compute and store the game values. Default is `False`.
+        verbose: Whether to print additional information. Default is `False`.
+        normalize_loaded: Whether to normalize the loaded game values. Default is `True`.
+
+    Returns:
+        The hyperparameter importance game, the name of the game, and the player names.
+    """
+    benchmark = benchmark_set.BenchmarkSet(benchmark_name)
+    if instance is None:
+        instance = benchmark.instances[0]
+
+    # get game_name
+    if game_type == "universal":
+        game_name = _get_game_name(
+            game_type, benchmark_name, metric=metric, n_configs=n_configs, random_state=random_state
+        )
+    else:
+        game_name = _get_game_name(
+            game_type,
+            benchmark_name,
+            metric=metric,
+            instance=instance,
+            n_configs=n_configs,
+            random_state=random_state,
+        )
+
+    # check if the game is already stored
+    game_path = os.path.join(GAME_STORAGE_DIR, f"{game_name}.npz")
+    name_file = os.path.join(GAME_STORAGE_DIR, f"names_{benchmark_name}.txt")
+    if os.path.exists(game_path) and os.path.exists(name_file):
+        game = shapiq.Game(path_to_values=game_path, verbose=verbose, normalize=normalize_loaded)
+        player_names = open(name_file).read().splitlines()
+        return game, game_name, player_names
+
+    # set up the game from parameters
+    if game_type == "universal":
+        game = UniversalHyperparameterImportanceGame(
+            benchmark,
+            metric=metric,
+            n_configs=n_configs,
+            random_state=random_state,
+            verbose=verbose,
+        )
+    elif game_type == "global":
+        benchmark.set_instance(instance)
+        game = GlobalHyperparameterImportanceGame(
+            benchmark,
+            metric=metric,
+            n_configs=n_configs,
+            random_state=random_state,
+            verbose=verbose,
+        )
+    elif game_type == "local":
+        benchmark.set_instance(instance)
+        optimal_cfg, _ = _find_optimal_configuration(
+            benchmark, metric=metric, random_state=random_state, n_configs=n_configs
+        )
+        game = LocalHyperparameterImportanceGame(benchmark, metric, optimal_cfg, verbose=verbose)
+    elif game_type == "universal-local":
+        optimal_cfg_list = []
+        for instance in tqdm(benchmark.instances):
+            benchmark.set_instance(instance)
+            optimal_cfg, _ = _find_optimal_configuration(
+                benchmark, metric=metric, random_state=random_state, n_configs=n_configs
+            )
+            optimal_cfg_list.append(optimal_cfg)
+        game = UniversalLocalHyperparameterImportanceGame(
+            benchmark, metric, optimal_cfg_list, verbose=verbose
+        )
+    else:
+        raise ValueError(f"Invalid game type: {game_type}")
+
+    # pre-compute and save the game values
+    if pre_compute:
+        game.precompute()
+        game.save_values(game_path)
+
+    # save the player names
+    with open(name_file, "w") as f:
+        f.write("\n".join(game.tunable_hyperparameter_names))
+    player_names = game.tunable_hyperparameter_names
+
+    return game, game_name, player_names
