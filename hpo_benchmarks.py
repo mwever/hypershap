776398da76c46ba9cf113322b57d41bdb2e1015f
mwever <m.wever@ai.uni-hannover.de>
2025-01-16 17:58:37 +0100

    Added continuous SMAC explanations to the repo

--- hpo_benchmarks.py
+++ hpo_benchmarks.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 import ConfigSpace
 import numpy as np
 from ConfigSpace import Configuration, __all__
+from smac import Scenario
+from smac.facade import AbstractFacade
+from smac.utils.configspace import convert_configurations_to_array
 
 
 class HyperparameterOptimizationBenchmark(ABC):
@@ -143,6 +146,12 @@ class YahpoGymBenchmark(HyperparameterOptimizationBenchmark):
         obj = None
         # iterate over the configurations in the list of configurations and return the maximum/minimum objective value
         for config in configuration:
+            base_cfg = self.get_default_config()
+            config = dict(config)
+            for k in base_cfg.keys():
+                if k not in config:
+                    config[k] = base_cfg[k]
+
             success = False
             while not success and len(config) > 0:
                 try:
@@ -162,6 +171,7 @@ class YahpoGymBenchmark(HyperparameterOptimizationBenchmark):
                     string_error = repr(e)
                     param_to_del = string_error.split("hyperparameter '")[1].split("' must")[0]
                     config.pop(param_to_del, None)
+                    print(e)
 
         # restore instance index originally set
         if change_back and (type(self.instance_idx) is int or type(self.instance_idx) is str):
@@ -361,3 +371,49 @@ class JAHSBenchmark(HyperparameterOptimizationBenchmark):
     def get_instances(self):
         return [ "1" ]
 
+class SMACAnalysisBenchmark(HyperparameterOptimizationBenchmark):
+
+    def __init__(self, smac: AbstractFacade, scenario: Scenario):
+        self.smac = smac
+        self.scenario = scenario
+
+
+    def get_list_of_tunable_hyperparameters(self):
+        return [cfg for cfg in self.scenario.configspace.get_hyperparameter_names()]
+
+    def get_list_of_nontunable_hyperparameters(self):
+        return []
+
+    def set_instance(self, instance):
+        print("WARNING: instance information is ignored in SMACAnalysisBenchmark")
+
+    def get_num_instances(self):
+        return 1
+
+    def sample_configurations(self, n=1, random_state=None):
+        cfgs = self.scenario.configspace.sample_configuration(n)
+        if n == 1:
+            return [cfgs.get_dictionary()]
+        else:
+            return [x.get_dictionary() for x in cfgs]
+
+    def get_opt_space(self):
+        return self.scenario.configspace
+
+    def evaluate(self, configuration, instance=None):
+        if type(configuration) is dict:
+            configuration = [configuration]
+
+        cfg_list = [Configuration(configuration_space=self.scenario.configspace, values=cfg) for cfg in configuration]
+        array = convert_configurations_to_array(cfg_list)
+        pred = (-1) * self.smac._model.predict(array)
+        return pred
+
+    def get_default_config(self):
+        return self.scenario.configspace.get_default_configuration().get_dictionary()
+
+    def get_default_config_performance(self, instance=None):
+        return self.evaluate(self.get_default_config())
+
+    def get_instances(self):
+        return []
--- hpo_games.py
+++ hpo_games.py
@@ -1,5 +1,4 @@
 import copy
-import time
 from abc import ABC, abstractmethod
 from typing import Optional
 
@@ -79,11 +78,17 @@ class AblationSetHPIGame(AbstractHPIGame):
             self,
             hpoBenchmark: HyperparameterOptimizationBenchmark,
             optimized_cfg_list,
+            reference_cfg_list=None,
             aggregator=lambda x: np.array(x).mean(),
             random_state=None,
             verbose: bool = False,
     ) -> None:
         self.optimized_cfg_list = optimized_cfg_list
+        if reference_cfg_list is not None:
+            self.reference_cfg_list = reference_cfg_list
+            assert len(self.reference_cfg_list) == len(self.optimized_cfg_list), "Optimized config list must have same length as reference config list"
+        else:
+            self.reference_cfg_list = [hpoBenchmark.get_default_config()] * len(self.optimized_cfg_list)
         self.aggregator = aggregator
         super().__init__(hpoBenchmark, random_state, verbose)
 
@@ -91,23 +96,47 @@ class AblationSetHPIGame(AbstractHPIGame):
         return self.aggregator(self.hpoBenchmark.get_default_config_performance())
 
     def evaluate_single_coalition(self, coalition: np.ndarray):
-        cfgs = self.blind_parameters_according_to_coalition(self.optimized_cfg_list, coalition)
+        cfgs = self.blend_parameters_according_to_coalition(self.optimized_cfg_list, coalition)
         obj = list()
-
         for i in range(self.hpoBenchmark.get_num_instances()):
             obj += [self.hpoBenchmark.evaluate(cfgs, instance=i)]
         agg_value = self.aggregator(obj)
         return agg_value
 
+    def blend_parameters_according_to_coalition(self, cfgs, coalition):
+        cfgs = copy.deepcopy(cfgs)
+        list_of_hyperparams_to_blind = np.array(self.hpoBenchmark.get_list_of_tunable_hyperparameters())[
+            (1 - coalition).astype(bool)
+        ]
+        list_of_hyperparams_to_blind = np.concatenate(
+            (
+                list_of_hyperparams_to_blind,
+                np.array(self.hpoBenchmark.get_list_of_nontunable_hyperparameters()),
+            )
+        )
+
+        reference = copy.deepcopy(self.reference_cfg_list)
+        default = self.hpoBenchmark.get_default_config()
+
+        for i, cfg in enumerate(cfgs):
+            for key in cfg.keys():
+                if key in list_of_hyperparams_to_blind and key in default.keys():
+                    cfg[key] = reference[i][key]
+        return cfgs
 
 class AblationHPIGame(AblationSetHPIGame):
-    def __init__(self, hpoBenchmark: HyperparameterOptimizationBenchmark, instance, optimized_cfg, random_state=None,
+    def __init__(self, hpoBenchmark: HyperparameterOptimizationBenchmark, instance, optimized_cfg, reference_cfg=None, random_state=None,
                  verbose: bool = False):
         self.optimized_cfg = optimized_cfg
+        self.reference_cfg = reference_cfg
         hpoBenchmark.set_instance(instance)
         assert hpoBenchmark.get_num_instances() == 1, "Number of instances cannot exceed 1 for ablations."
 
-        super().__init__(hpoBenchmark, optimized_cfg_list=[optimized_cfg], random_state=random_state, verbose=verbose)
+        reference_cfg_list = [reference_cfg] if reference_cfg is not None else None
+        super().__init__(hpoBenchmark, optimized_cfg_list=[optimized_cfg], reference_cfg_list=reference_cfg_list, random_state=random_state, verbose=verbose)
+
+        print(self.optimized_cfg_list)
+        print(self.reference_cfg_list)
 
 
 class TunabilityHPIGame(AbstractHPIGame):
