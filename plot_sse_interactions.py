732c6a45bca74a16ad44fd33b76df7176b6f54d0
Maximilian <maximilian.muschalik@gmail.com>
2025-01-22 15:58:41 +0100

    plots different budgets

--- plot_sse_interactions.py
+++ plot_sse_interactions.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parameter_names = abbreviate_player_names(parameter_names)
 
     # budgets = [25, 50, 100, 200, 400, 800, 1600, 3200]
-    budgets = [25, 50, 100, 200]
+    budgets = [200, 800, 1600, 3200]
     interactions_list = []
     for budget in budgets:
 
